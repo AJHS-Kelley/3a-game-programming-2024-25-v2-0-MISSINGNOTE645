1025cd901eec5d130889a3ce44d87e75481a1624
MISSINGNOTE645 <s20024169@students.duvalschools.org>
2024-09-12 13:21:03 -0400

    MY CODE

--- 01_gaming_exercises/00_awarding_extra_lives/awarding_extra_lives.py
+++ 01_gaming_exercises/00_awarding_extra_lives/awarding_extra_lives.py
@@ -1,8 +1,18 @@
 # Awarding extra lives, Calvin young, v.0.0
 
-
 lives = 3
-score = 10000
+score = 100001
+name = "Calvin"
+
+
+print(f"Hello {name}!  you scord {score} ponits.\n")
+
+
+# CHARACTER REFERENCE
+# CURLY BRACES {}
+# BRACKETS []
+# ANGLE-BRACKETS <>
+# PARENTHESES ()
 
 # Allow user to input the score AS AN INTGER
 
@@ -17,7 +27,14 @@ score = 10000
 
 if score <= 10000:
     print("Lose a life")
+    lives -= 1
 elif score < 100001:
     print("Give 1 extra life")
+    lives += 1
 elif score > 100000:
     print("Give 2 extra lives")
+    lives += 2
+
+print("lives: " + str(lives))
+print("score: " + str(score))
+
