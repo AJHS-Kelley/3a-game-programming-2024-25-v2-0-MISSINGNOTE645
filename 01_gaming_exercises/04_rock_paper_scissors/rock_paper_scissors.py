23d231b7459a0792b2489f71e0ed825dc36e3cbb
MISSINGNOTE645 <s20024169@students.duvalschools.org>
2024-09-18 11:50:32 -0400

    v0.7.5

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
@@ -1,3 +1,15 @@
-Please delete this file after cloning the repository. 
+# Rock, paper, Scissiors bt Calvin Young, v0.7
 
-Then commit the changes to GitHub after deleting ALL of the placeholder files. 
+# MODULE IMPORTS
+import random
+
+
+#DATA STRUCTURES -- PLAYER
+player_name = "test_player"
+player_score = 0
+player_choice = None
+
+ 
+ #DATA STRUCTURES -- CPU
+cpu_score = 0
+cpu_choice = None
