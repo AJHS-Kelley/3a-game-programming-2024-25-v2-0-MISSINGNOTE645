427a1f7230c70b235ce4c86f3dd562099e15900c
MISSINGNOTE645 <s20024169@students.duvalschools.org>
2024-09-20 11:50:16 -0400

    Update rock_paper_scissors.py
    fixed the or problem, and the player score

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
@@ -27,7 +27,7 @@ else:
 
 # THE RULES
 print("""
-welcome to the Rock, Paper, Scissors Robot
+welcome {player_name} the Rock, Paper, Scissors Robot
 Its Time To Play ROCK, PAPER, SCISSORS.
       
 You have probably played lost in this MANY TIME and know the rules but for those who dont know
@@ -36,7 +36,7 @@ The CPU will select ROCK, PAPER, OR SCISSORS at Random
       
 1) ROCK OBLITERATES SCISSORS
 2) SCISSORS SLICES PAPER
-3) PAPER DESTROYS ROCK
+3) PAPER KIDNAPS ROCK
 """)
 
 # MULI-LINE STRINGS CAN BE USED AS BIG COMMENTS
@@ -47,20 +47,32 @@ putting a # in front
 """
 
 # MAIN GAME LOOP
-while player_name < 5 and cpu_score < 5:
+while player_score < 5 and cpu_score < 5:
     print(f"{player_name} you have {player_score} points. \nThe CPU has {cpu_score} points\n")
     player_choice = input("Please enter rock, paper or scissors and press enter\n").lower()
-    if player_choice != "rock" or player_choice != "paper" or player_choice != "scissors":
+    if player_choice != "rock" and player_choice != "paper" and player_choice != "scissors":
         player_choice = input("Please enter rock, paper or scissors and press enter\n").lower()
-        if player_choice != "rock" or player_choice != "paper" or player_choice != "scissors":
+        if player_choice != "rock" and player_choice != "paper" and player_choice != "scissors":
             print("You are not folleing directions.  Please try again.\n")
             exit()
-            print(f"you have chosen {player_choice}.\n")
+        print(f"you have chosen {player_choice}.\n")
     else:
         print(f"you have chosen {player_choice}.\n")
 
     # print the current score for CPU and Player
     # let player select rock, paper, scissors
+    cpu_choice = random.randint(0,2) # randomly select 0, 1 or 2.
+    if cpu_choice == 0:
+        cpu_choice = "rock"
+    elif cpu_choice == 1:
+        cpu_choice = "scissors"
+    elif cpu_choice == 2:
+        cpu_choice = "paper"
+    else:
+        print("Some how you broke the cpu's choice:/. \n please restart program if you can even do that")
+        exit()
+    print(f"CPU Choice: {cpu_choice}")
+
     # let CPU select choice at random
     # Compare player vhoice to cpu choice
     # print results to the screen
