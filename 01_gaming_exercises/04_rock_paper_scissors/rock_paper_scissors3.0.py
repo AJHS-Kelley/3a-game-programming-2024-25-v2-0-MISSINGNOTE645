b408e2aefb3c57e3dbfeed1a1f1ad5203fe49f32
MISSINGNOTE645 <s20024169@students.duvalschools.org>
2024-10-16 13:19:56 -0400

    v3.0 update 1

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors3.0.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors3.0.py
@@ -13,42 +13,42 @@ cpu_score = 0
 cpu_choice = None
 
 # PLAYER NAME INPUT
-player_name = input("Please type yourn name here and press enter.\n")
-print(f"Hello {player_name}!\n")
-isCorrect = input ("Is that correct? Type yes or no and press enter.\n").lower()
+def player_name() -> str: # Function Signature -- name of function, (arguments if any)
+    """Gets the name form the player and returns it."""
+    # The line above is a DOCSTRING, it gives a brief example of what the function does.
+    player_name = input("Please type your name here and press enter.\n")
+    print(f"Hello {player_name}!\n")
+    isCorrect = input ("Is that correct? Type yes or no and press enter.\n").lower()
+    if isCorrect == "yes":
+        print(f"ok {player_choice}, let's play Rock, Paper, Scissors!\n")
+    else:
+        player_name = input("please type your name and press enter.\n")
+    return player_name
 
-# .lower() can turn ALL input into lowercase
-# .upper() can turn ALL input into UPPERCASE
-
-if isCorrect == "yes":
-    print(f"ok {player_choice}, let's play Rock, Paper, Scissors!\n")
-else:
-    player_name = input("please type your name and press enter.\n")
+# CALLING THE FUNCTION
+player_name = player_name() 
 
 # THE RULES
-print(f"""
-welcome {player_name} the Rock, Paper, Scissors Robot
-Its Time To Play ROCK, PAPER, SCISSORS.
-      
-You have probably played lost in this MANY TIME and know the rules but for those who dont know
-You will pick from ROCK, PAPER, OR SCISSORS.
-The CPU will select ROCK, PAPER, OR SCISSORS at Random
-      
-1) ROCK OBLITERATES SCISSORS
-2) SCISSORS SLICES PAPER
-3) PAPER KIDNAPS ROCK
-""")
+def rules() -> None:
+    """This function prints the rules of rock, paper, scissors."""
+    print(f"""
+    welcome {player_name} the Rock, Paper, Scissors Robot
+    Its Time To Play ROCK, PAPER, SCISSORS.
+        
+    You have probably played lost in this MANY TIME and know the rules but for those who dont know
+    You will pick from ROCK, PAPER, OR SCISSORS.
+    The CPU will select ROCK, PAPER, OR SCISSORS at Random
+        
+    1) ROCK OBLITERATES SCISSORS
+    2) SCISSORS SLICES PAPER
+    3) PAPER KIDNAPS ROCK
+    """)
+    # Does anouther part of this program need to access this infomation?
+    #IF YES, YOU MUST HAVE A return STATMENT
+    #IF NO, A return STATMENT IS NOT REQUIRED
 
-# MULI-LINE STRINGS CAN BE USED AS BIG COMMENTS
-"""
-Anything in between the sets of duoble-quotes is just ignored.
-If you you need to write large comments, it's easier to use multi-line strings than
-putting a # in front
-"""
-
-# MAIN GAME LOOP
-while player_score < 5 and cpu_score < 5:
-    print(f"{player_name} you have {player_score} points. \nThe CPU has {cpu_score} points\n")
+def player_choice() -> str:
+    """This function allows player to chose rock, paper, scissors"""
     player_choice = input("Please enter rock, paper or scissors and press enter\n").lower()
     if player_choice != "rock" and player_choice != "paper" and player_choice != "scissors":
         player_choice = input("Please enter rock, paper or scissors and press enter\n").lower()
@@ -58,8 +58,10 @@ while player_score < 5 and cpu_score < 5:
         print(f"you have chosen {player_choice}.\n")
     else:
         print(f"you have chosen {player_choice}.\n")
+    return player_choice
 
-    # let player select rock, paper, scissors
+def cpu_choice()-> str:
+    """This function allows CPU to chose rock, paper, scissors"""
     cpu_choice = random.randint(0,2) # randomly select 0, 1 or 2.
     if cpu_choice == 0:
         cpu_choice = "rock"
@@ -70,6 +72,9 @@ while player_score < 5 and cpu_score < 5:
     else:
         print("Some how you broke the cpu's choice:/. \n please restart program if you can even do that\n")
         exit()
+    return cpu_choice
+
+
     print(f"CPU Choice: {cpu_choice}")
 
     if player_choice == "rock"and cpu_choice == "paper":
