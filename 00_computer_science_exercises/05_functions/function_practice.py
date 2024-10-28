51158c7f96ebae555485de60d8c06e32124764b9
MISSINGNOTE645 <s20024169@students.duvalschools.org>
2024-10-28 12:05:05 -0400

    # Functions Practice, Calvin Young, v0,0

---
+++ 00_computer_science_exercises/05_functions/function_practice.py
@@ -0,0 +1,61 @@
+# Functions Practice, Calvin Young, v0,0
+
+import random
+
+def helloWorldMulti(): # FUNCTION STGNATURE
+    """This function will output Hello, World! in a language the user choose. """
+    # print a list of languages to the screen, at least three but no more than five.
+    print("""
+        Welcome to the Hello, World! Translator
+        Choose what language yopu would like to use.
+        [A]English
+        [B]Spanish
+        [Y]German
+        [X]Russian
+        """)
+    
+
+    # allow the user to select (imput) a choice for the language.
+    language = input("What language do you want?\n Pruss the button that matches the letter on the left.\n").lower()
+
+    # print "Hello, World!" to the screen in that language.
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
